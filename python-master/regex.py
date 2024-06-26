f55d70969d6940000989a42423f8de1fc084df0b
priyankarnd <priroychow@gmail.com>
2024-06-26 17:43:04 -0600

    Update regex.py

--- python-master/regex.py
+++ python-master/regex.py
@@ -6,6 +6,7 @@ findall:	Returns a list containing all matches
 search:	    Returns a Match object if there is a match anywhere in the string
 split:	    Returns a list where the string has been split at each match
 sub:	    Replaces one or many matches with a string
+Documentation: https://www.w3schools.com/python/python_regex.asp
 '''
 
 text = "This is Book"
@@ -15,6 +16,13 @@ if x:
     print("Yes, match found")
 else:
     print("No, match not found")
+
+y = re.search("is",text)
+
+if y:
+    print("Yes, match found")
+else:
+    print("No match found")
 
 txt = "The rain in India and Indian rain"
 x = re.findall("ai", txt)   # Occurrences
@@ -35,6 +43,10 @@ print(x)
 
 x = re.split("\s", txt, 3)
 print(x)
+
+split_term = '@'
+email = 'user@gmail.com'
+print(re.split(split_term,email))
 
 x = re.sub("\s","-", txt)
 print(x)
