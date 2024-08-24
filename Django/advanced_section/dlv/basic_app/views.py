42cb7789ec1701563bfda079e854be63ddef78a8
priyankarnd <priroychow@gmail.com>
2024-08-24 12:10:44 -0600

    Advanced Django

---
+++ Django/advanced_section/dlv/basic_app/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.generic import View,TemplateView,ListView,DetailView,CreateView,DeleteView,UpdateView
+from . import models
+from django.urls import reverse_lazy
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
+    # returns a list called: school_list by default
+    template_name = 'basic_app/school_list.html'
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    fields = {"name","principal","location"}
+    model = models.School
+
+class SchoolUpdateView(UpdateView):
+    fields = {"name","principal"}
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
+
